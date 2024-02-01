0bda93e40964a71b10a61e6e557be8dbef2b1e15
Zijian Zhang <doomspec@outlook.com>
2024-02-01 02:41:17 -0500

    chore: change show gui function name

--- test/temp/show_gui.py
+++ test/temp/show_gui.py
@@ -3,4 +3,4 @@ from evonote.transform.module_to_tree import get_tree_for_module
 import testing_module_multi
 
 tree = get_tree_for_module(testing_module_multi)
-tree.show_tree_gui()
+tree.show_tree_gui_old()
